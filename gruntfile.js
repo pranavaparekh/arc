f1adc1d7cf5a7f0ad0a104b610102c7806a47476
Amit Erandole <amit.erandole@gmail.com>
2015-03-07 20:29:14 +0530

    added gruntfile with sass and watch tasks

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    // Project Configuration
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            sass: {
+                files: ['src/sass/site/**/*.scss'],
+                tasks: 'sass'
+            },
+        },
+        sass: {
+            dist: {
+                options: {
+                  style: 'expanded'
+                },
+                files: {
+                    'public/assets/css/main.css': 'src/sass/site/main.scss'
+                }
+            }
+        },
+    });
+
+    //Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['sass', 'watch']);
+
+};
