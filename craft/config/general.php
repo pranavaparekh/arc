3fd85ca7106316044a4b9ff59451e87f3bac2fd9
Amit Erandole <amit.erandole@gmail.com>
2015-03-07 23:18:00 +0530

    added multi-environment variables

--- craft/config/general.php
+++ craft/config/general.php
@@ -7,20 +7,44 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
+// Ensure our urls have the right scheme
+define('URI_SCHEME', ( isset($_SERVER['HTTPS'] ) ) ? "https://" : "http://" );
+
+// The site url
+define('SITE_URL', URI_SCHEME . $_SERVER['SERVER_NAME'] . '/');
+
+// The site basepath
+define('BASEPATH', realpath(CRAFT_BASE_PATH . '/../') . '/');
+
 return array(
   '*' => array(
     'omitScriptNameInUrls' => true,
+
+    'environmentVariables' => array(
+      'siteUrl'  => SITE_URL,
+      'basePath' => BASEPATH
+    ),
   ),
 
-  'live' => array(
-    // Config overrides for our live environment
+  'cacheBustValue' => '20121017',
+
+  // Create a custom variable that we can use for an environment conditional
+  // We set the environment in index.php: live, dev, or local
+  // This setting assumes we set the environment name in our index.php file
+  // {% if craft.config.env == 'live' %} ... {% endif %}
+  'env' => CRAFT_ENVIRONMENT,
+
+  // Triggers
+  // If you wish to access the control panel via a different URL
+  // or change your page trigger for pagination, you can do so here.
+  // 'cpTrigger'       => 'arancadmin',
+
+  'stanly' => array(
+    'devMode' => true,
   ),
 
-  'staging' => array(
-    // Config overrides for our live environment
+  'amite' => array(
+    'devMode' => true,
   ),
 
-  'dev' => array(
-    // Config overrides for our dev environment
-  ),
 );
