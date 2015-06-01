60aad4cfe72484bbfacd0cdc9a3f16d46e56318d
Stanly Samuel <stanrulz69@yahoo.co.in>
2015-06-01 17:21:35 +0530

    Finished masthead

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         sass: {
             dist: {
                 options: {
-                  style: 'compressed', 
+                  style: 'expanded', 
                   // nested , compact , compressed , expanded
                   sourcemap: 'auto',
                 },
