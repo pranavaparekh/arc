340d4c2ab03befe3c9523b5caedaf643c29c0058
Amit Erandole <amit.erandole@gmail.com>
2015-08-13 09:19:42 +0530

    added search component and watching new directory for sass

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             sass: {
-                files: ['src/sass/site/**/*.scss'],
+                files: ['<%= pkg.src %>/*.scss', '<%= pkg.components %>/*.scss' ],
                 tasks: 'sass'
             },
         },
