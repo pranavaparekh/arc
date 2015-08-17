987633ab72a556cebd03338db412bc0218219248
Amit Erandole <amit.erandole@gmail.com>
2015-08-17 10:40:04 +0530

    added vfilter component and switched to includeJS craft tag

--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -1,20 +1,20 @@
-$('.media-grid-c').flexImages({rowHeight:100, container:'.media-grid-c__item', object:'.media-grid-c__item-image'});
+// $('.media-grid-c').flexImages({rowHeight:100, container:'.media-grid-c__item', object:'.media-grid-c__item-image'});
 
 // //Javascript for vertical tabs
 // var tabsFn = (function() {
-  
+
 //   function init() {
 //     setHeight();
 //   }
-  
+
 //   function setHeight() {
 //     var $tabPane = $('.tab-pane-vertical'),
 //         tabsHeight = $('.nav-tabs-vertical').height();
-    
+
 //     $tabPane.css({
 //       height: tabsHeight
 //     });
 //   }
-    
+
 //   $(init);
 // })();
