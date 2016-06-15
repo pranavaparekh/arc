bba41774643efb98a90ce2acd875b99f46757633
amite <amit.erandole@gmail.com>
2016-06-15 15:51:12 +0530

    setup areas of interest layouts

--- craft/config/routes.php
+++ craft/config/routes.php
@@ -40,6 +40,8 @@ return array(
    'knowledge-library/(?P<ctype>\w+(\-?\w+?)*)/authors/(?P<author>\w+\-?\w+?)' => 'layouts/kc/_author',
    'knowledge-library/(?P<ctype>\w+(\-?\w+?)*)/archives/(?P<year>\d{4})-(?P<month>\d{2})' => 'layouts/kc/_archive',
 
+   'special-reports/pp-form' => 'layouts/special-reports/pp-form',
+
    'search/results' => 'layouts/search/results',
 
    'thankyou-(?P<practiceType>\w+\-?\w+?\-?\w+?)' => 'layouts/thank-you/_entry',
