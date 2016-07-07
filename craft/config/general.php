6875d58e2cd50982a1ea54fb3334d655593731df
amite <amit.erandole@gmail.com>
2016-07-07 09:44:53 +0530

    swtiched devmode true on production

--- craft/config/general.php
+++ craft/config/general.php
@@ -56,7 +56,7 @@ return array(
     'assetsImagesUrl' => '/assets/images',
   ),
   'staging' => array(
-    'devMode' => false,
+    'devMode' => true,
     'cache' => true,
     'assetsCssUrl' => '/assets/css',
     'assetsJsUrl' => '/assets/js',
