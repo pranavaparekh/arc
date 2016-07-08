48db2526f3c18c519eaeaf5841a8fc87f08aa5c2
amite <amit.erandole@gmail.com>
2016-07-08 07:01:52 +0530

    added papertrail plugin

---
+++ craft/config/papertrail.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Papertrail Configuration
+ */
+
+return array(
+
+    // Default
+    '*' => array(
+        'papertrailHost' => 'logs2.papertrailapp.com',
+        'papertrailPort' => 22660,
+        'appendLevel' => false,
+        'prependLevel' => true,
+    ),
+
+);
