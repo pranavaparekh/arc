3c000a2a0a3bdde671e26d3dda8156e40e81e621
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-08 18:49:09 +0530

    add waybeo

--- public/lp/research-on-the-go/index.php
+++ public/lp/research-on-the-go/index.php
@@ -1,7 +1,8 @@
 <?php
 include('_components/_dochead.html');
 include('_components/_nav.html');
-include('_components/_masthead2.html');
+// include('_components/_masthead2.html');
+include('_components/_masthead-form.html');
 include('_components/_intro.html');
 include('_components/_assignment-box.html');
 include('_components/_footer.html');
