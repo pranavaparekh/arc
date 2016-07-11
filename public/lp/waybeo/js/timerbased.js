296760890e5697abd9b352ab858c39799e777671
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-11 19:46:05 +0530

    removed IN

--- public/lp/waybeo/js/timerbased.js
+++ public/lp/waybeo/js/timerbased.js
@@ -69,7 +69,7 @@ function clearStatusbottom() {
 }
 
 Waybeo.Utils.getIpCountry(function(l) {
-    var _AllowedSouthAmericanCountries = ['US', 'IN'];
+    var _AllowedSouthAmericanCountries = ['US'];
     if($.inArray(l.country_code, _AllowedSouthAmericanCountries) != -1) {
        Waybeo.CTC.Init({
             hash: '56bd928aa3e24',
