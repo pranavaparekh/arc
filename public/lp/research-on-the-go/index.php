1a3c13863839c0d64298926eedc6aa59b1006cc8
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-18 14:24:19 +0530

    added new module

--- public/lp/research-on-the-go/index.php
+++ public/lp/research-on-the-go/index.php
@@ -1,9 +1,11 @@
 <?php
 include('_components/_dochead.html');
 include('_components/_nav.html');
+include('_components/_tabbed-slider-carousel.html');
 // include('_components/_masthead2.html');
-include('_components/_masthead-form.html');
-include('_components/_intro.html');
-include('_components/_assignment-box.html');
+// include('_components/_masthead-form.html');
+// include('_components/_intro.html');
+// include('_components/_assignment-box.html');
+
 include('_components/_footer.html');
 ?>
