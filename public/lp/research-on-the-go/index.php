c7cad9e81d5cbea803039f23fe3661fcebba8255
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-18 19:50:07 +0530

    added vertical tabs

--- public/lp/research-on-the-go/index.php
+++ public/lp/research-on-the-go/index.php
@@ -2,6 +2,7 @@
 include('_components/_dochead.html');
 include('_components/_nav.html');
 include('_components/_tabbed-slider-carousel.html');
+include('_components/_simple-vertical-tab.html');;
 // include('_components/_masthead2.html');
 // include('_components/_masthead-form.html');
 // include('_components/_intro.html');
