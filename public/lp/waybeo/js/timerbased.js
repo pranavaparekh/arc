c2b6642e6961d223e8307cc07278d98e35b1013f
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-22 12:25:10 +0530

    Activated timerbased for Middle East and Show the number for US

--- public/lp/waybeo/js/timerbased.js
+++ public/lp/waybeo/js/timerbased.js
@@ -68,7 +68,7 @@ function clearStatusbottom() {
             .removeClass('timer');
 }
 
-//timerbased popup
+//timerbased popup for US
 
 // Waybeo.Utils.getIpCountry(function(l) {
 //     var _AllowedSouthAmericanCountries = ['US'];
@@ -84,23 +84,49 @@ function clearStatusbottom() {
 //     }
 // });
 
+// function makecallTimer(_phone) {
+//     Waybeo.CTC.MakeCall({
+//         'hash': '56bd928aa3e24',
+//         'route_hash': '56bd8379cf7b9',
+//         'callerid_hash': '573422281ad9d',
+//         'contact_number': _phone
+//     }, eventCallBack);
+// }
+
+// timerbased popup for Middle East
+Waybeo.Utils.getIpCountry(function(l) {
+    var _AllowedSouthAmericanCountries = ['SA','AE', 'QA', 'JO', 'IL', 'OM', 'KW', 'BH'];
+    if($.inArray(l.country_code, _AllowedSouthAmericanCountries) != -1) {
+       Waybeo.CTC.Init({
+            hash: '5780845c74a17',
+            autoPopup: {
+                aggressive: true,
+                timer: 5,
+                trigger: showTimerPopup
+            }
+        });
+    }
+});
+
+function makecallTimer(_phone) {
+    Waybeo.CTC.MakeCall({
+        'hash': '5780845c74a17',
+        'route_hash': '5780813adc8ca',
+        'callerid_hash': '5780845c931b7',
+        'contact_number': _phone
+    }, eventCallBack);
+}
+
+// Show number at top navigation
 Waybeo.Utils.getIpCountry(function(location) {
     var _location = location.country_code;
-    if (_location == 'IN') {
+    if (_location == 'US') {
          $( "#int-num" ).show();
          var _intnum = $("#int-num-display").data('telus');
          $("#int-num-display").html(_intnum);
         }
     });
 
-function makecallTimer(_phone) {
-    Waybeo.CTC.MakeCall({
-        'hash': '56bd928aa3e24',
-        'route_hash': '56bd8379cf7b9',
-        'callerid_hash': '573422281ad9d',
-        'contact_number': _phone
-    }, eventCallBack);
-}
 
 var captcha = '', timer = '';
 function eventCallBack(event, data) {
