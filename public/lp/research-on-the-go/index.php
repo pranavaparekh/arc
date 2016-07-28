2bdc93d39698e2f959cc74ff59022654fa02a1f9
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-07-28 12:04:03 +0530

    updated banner

--- public/lp/research-on-the-go/index.php
+++ public/lp/research-on-the-go/index.php
@@ -1,12 +1,14 @@
 <?php
 include('_components/_dochead.html');
 include('_components/_nav.html');
+include('_components/_masthead-bigbanner.html');
+include('_components/_intro.html');
 include('_components/_tabbed-slider-carousel.html');
 include('_components/_simple-vertical-tab.html');
 include('_components/_CTA.html');
-// include('_components/_masthead2.html');
+
 // include('_components/_masthead-form.html');
-// include('_components/_intro.html');
+
 // include('_components/_assignment-box.html');
 
 include('_components/_footer.html');
