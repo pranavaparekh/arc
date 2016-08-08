4f2299a47767798e3192bf9fac2ba96315cc4879
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-08-08 16:27:47 +0530

    added dochead and footer in global component

--- public/lp/research-on-the-go/index.php
+++ public/lp/research-on-the-go/index.php
@@ -1,16 +1,11 @@
 <?php
-include('_components/_dochead.html');
-include('_components/_nav.html');
+include('../_components/_global/_dochead.html');
+include('../_components/_global/_nav.html');
 include('_components/_masthead-bigbanner.html');
 include('_components/_intro.html');
 include('_components/_tabbed-slider-carousel.html');
 include('_components/_simple-vertical-tab.html');
 include('_components/_CTA.html');
 include('_components/_CTA_form.html');
-
-// include('_components/_masthead-form.html');
-
-// include('_components/_assignment-box.html');
-
-include('_components/_footer.html');
+include('../_components/_global/_footer.html');
 ?>
