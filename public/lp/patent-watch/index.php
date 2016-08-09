0492517aa4b7aab837ed9a1930d12fefb68ed5bd
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-08-09 14:04:53 +0530

    change tree structure for LP

---
+++ public/lp/patent-watch/index.php
@@ -0,0 +1,6 @@
+<?php
+include('../_components/_global/_dochead.html');
+include('../_components/_global/_nav.html');
+include('../_components/_masthead-bigbanner.html');
+include('../_components/_global/_footer.html');
+?>
