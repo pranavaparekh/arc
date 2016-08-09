0270a9042ec237bf491db3201111ce2a17651e44
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-08-09 16:12:26 +0530

    added new components for stats track record

--- public/lp/patent-watch/index.php
+++ public/lp/patent-watch/index.php
@@ -2,5 +2,7 @@
 include('../_components/_global/_dochead.html');
 include('../_components/_global/_nav.html');
 include('../_components/_masthead-bigbanner.html');
+include('../_components/_intro-2col-2row.html');
+include('../_components/_track-records.html');
 include('../_components/_global/_footer.html');
 ?>
