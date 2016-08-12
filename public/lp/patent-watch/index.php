694c1bd94bc160c142590a16d1ae7f06147ab132
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-08-12 11:31:30 +0530

    updated content

--- public/lp/patent-watch/index.php
+++ public/lp/patent-watch/index.php
@@ -1,7 +1,7 @@
 <?php
 include('../_components/_global/_dochead.html');
 include('../_components/_global/_nav.html');
-include('../_components/_masthead-bigbanner.html');
+include('../_components/_masthead-smallbanner.html');
 include('../_components/_intro-2col-2row.html');
 include('../_components/_track-records.html');
 include('../_components/_3col-services.html');
