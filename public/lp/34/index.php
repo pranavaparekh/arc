51f91426a5ada1cbbb55a13af7f7b0518e7183c9
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-11-09 20:52:43 +0530

    added new website page 34

---
+++ public/lp/34/index.php
@@ -0,0 +1,10 @@
+<?php
+
+include('_includes/dochead.html');
+include('_includes/menu.html');
+include('_includes/masthead.html');
+include('_includes/landing.html');
+include('_includes/round-icon-badge.html');
+include('_includes/_team.html');
+include('_includes/_CTA-form.html');
+include('_includes/footer.html');
