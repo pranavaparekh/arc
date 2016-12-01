ed4a5c4235cf9851f82892d9dfe0200fed35d788
sandeshsmartguy <sandesh.smartguy@gmail.com>
2016-12-01 08:36:56 +0530

    updated geo

--- public/lp/my.php
+++ public/lp/my.php
@@ -2,8 +2,5 @@
 $ip = $_SERVER['REMOTE_ADDR'];
 
 echo $ip;
-  function ip_details($ip) {
-$json = file_get_contents("http://ipinfo.io/{$ip}/geo");
-$details = json_decode($json);
-return $details;
+
 ?>
